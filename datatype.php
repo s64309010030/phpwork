a73edb237721363812e05b637898d033be3b81ff
s64309010030 <116244796+s64309010030@users.noreply.github.com>
2022-10-20 16:23:44 +0700

    first commit

---
+++ datatype.php
@@ -0,0 +1,26 @@
+<?php
+
+    //String
+    $grade = "A";
+    $subject = "HTML";
+    $name = 'Busya';
+    $age = "19";
+
+    //Integer Number
+    $timeout = 30;
+    $score = 99;
+    $loan = -19;
+    $money = +88;
+
+    //floating point
+    $gpa = 3.33;
+    $inits = 0.25;
+    $temp = -2.22;
+    $lumen = +7600.99;
+    $maths = 2e10;
+    echo $maths;
+
+    //Boolean
+    $famale = true;
+    $male = false;
+?>
---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+    echo "busya phromchan<br>";
+    //echo "Thailand";
+    #echo "Asia";
+    /*echo "world<br>";
+    echo "Universe";*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Information Technology <br>
+    <?php
+        echo "Pattani;"
+    ?>
+</body>
+</html>
